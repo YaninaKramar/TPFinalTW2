5e0603d4aa3d661d792cb1fa825814f480cb8496
tomycernik <cerniktomy@gmail.com>
2025-06-26 22:26:47 -0300

    feat: Implementación módulo productos por Tomas Cernik
    - Creado ProductoService con método getProducto()
    - Implementado componente DetailProducto
    - Agregada interface Producto
    - Configuradas rutas para detalle-producto/:id
    - Setup de environments para API
    - Desactivado strict mode en TypeScript

---
+++ herrashop/src/app/modules/productos/pages/detail-producto/detail-producto.ts
@@ -0,0 +1,46 @@
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { ProductoService } from '../../../../services/producto.service';
+import { ActivatedRoute } from '@angular/router';
+import { Producto } from '../../interfaces/producto.interface';
+
+@Component({
+  selector: 'app-detail-producto',
+  imports: [],
+  templateUrl: './detail-producto.html',
+  styleUrl: './detail-producto.css'
+})
+export class DetailProducto implements OnInit, OnDestroy {
+
+  productoService = inject(ProductoService); 
+
+  activatedRouter = inject(ActivatedRoute);
+
+  id: number | null = null;
+
+  producto: Producto | undefined;
+
+  ngOnInit() {
+    this.id = Number(this.activatedRouter.snapshot.paramMap.get('id'))
+    this.getProducto();  
+  }
+
+  ngOnDestroy(): void {
+    
+  }
+
+  getProducto() {
+      this.productoService.getProducto(this.id).subscribe(
+        {
+          next : (data)=>{
+            this.producto = data;
+            console.log(data);
+          }, 
+
+        error: (err) => {},
+
+        complete: () => {}
+      }
+    );
+  }
+
+}
